c30e52bf760684a2ad000b947df3d6bab9eca53b
IvAlyosha <altyshka009@mail.ru>
2025-04-09 23:07:44 +0300

    commit_after_rewiew_1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from django.http import Http404
 
 posts = [
     {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
+        "id": 0,
+        "location": "Остров отчаянья",
+        "date": "30 сентября 1659 года",
+        "category": "travel",
+        "text": """Наш корабль, застигнутый в открытом море
                 страшным штормом, потерпел крушение.
                 Весь экипаж, кроме меня, утонул; я же,
                 несчастный Робинзон Крузо, был выброшен
                 полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
+                который назвал островом Отчаяния.""",
     },
     {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
+        "id": 1,
+        "location": "Остров отчаянья",
+        "date": "1 октября 1659 года",
+        "category": "not-my-day",
+        "text": """Проснувшись поутру, я увидел, что наш корабль сняло
                 с мели приливом и пригнало гораздо ближе к берегу.
                 Это подало мне надежду, что, когда ветер стихнет,
                 мне удастся добраться до корабля и запастись едой и
@@ -27,39 +28,41 @@ posts = [
                 Мне всё думалось, что, останься мы на корабле, мы
                 непременно спаслись бы. Теперь из его обломков мы могли бы
                 построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
+                гиблого места.""",
     },
     {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
+        "id": 2,
+        "location": "Остров отчаянья",
+        "date": "25 октября 1659 года",
+        "category": "not-my-day",
+        "text": """Всю ночь и весь день шёл дождь и дул сильный
                 порывистый ветер. 25 октября.  Корабль за ночь разбило
                 в щепки; на том месте, где он стоял, торчат какие-то
                 жалкие обломки,  да и те видны только во время отлива.
                 Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
+                укутывал их, чтобы не испортились от дождя.""",
     },
 ]
 
 
 def index(request):
 
-    template = 'blog/index.html'
-    context = {'posts': posts}
-    return render(request, template, context)
+    return render(request, "blog/index.html", {"posts": posts})
 
 
-def post_detail(request, id):
+def post_detail(request, post_id):
 
-    context = {'post': posts[id], 'lines': posts[id]['text'].splitlines()}
-    template = 'blog/detail.html'
-    return render(request, template, context)
+    post = None
+    for p in posts:
+        if p["id"] == post_id:
+            post = p
+            break
+    if post is None:
+        raise Http404("Пока такой пост не опубликовали:(")
+    return render(request, "blog/detail.html", {"post": post})
 
 
 def category_posts(request, category_slug):
 
-    template = 'blog/category.html'
-    context = {'category_slug': category_slug}
-    return render(request, template, context)
+    return render(request, "blog/category.html",
+                  {"category_slug": category_slug})
--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-00#dy%heu2^*)(_1fr9-un)2+i%bl%rsd+@1=)q4^3iy^t&9ot
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
 
